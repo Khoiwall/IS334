3cd83554cddc4c35a9c61d19308ac65e22174030
Metastream AS <khoitr190@gmail.com>
2023-05-19 00:32:36 +0700

    update today

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomePage() {
         title="Trang chủ"
         description={"Bán nến thơm"}
         image={
-          "https://ucarecdn.com/36152da1-0d6e-4016-8e88-509ca124451e/image.png"
+          "https://i0.wp.com/thescentnote.com/wp-content/uploads/2023/02/nen-thom-trang-tri-2.jpg?fit=1024%2C742&ssl=1"
         }
         url={process.env.NEXT_PUBLIC_METASTREAM}
       />
